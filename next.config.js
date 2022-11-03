a1166a1fddf66faa168c97accca066b5cf076fa9
Kevin Adu-Poku <kevin.adupoku@outlook.com>
2022-11-03 16:38:56 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,19 @@
 module.exports = {
-reactStrictMode: true,
+/* reactStrictMode: true,
    swcMinify: true
  }
  const withBundleAnalyzer = require('@next/bundle-analyzer')({ 
    enabled: process.env.ANALYZE === 'true'
  })
  module.exports = withBundleAnalyzer({})
+*/
+async redirects() {
+    return [
+      {
+        source: "/((?!maintenance).*)",
+        destination: "/maintenance.html",
+        permanent: false,
+      },
+    ];
+  },
+};
